57b156b2104803d3d4c085f843eb985298380d77
Ciovino <tarantino.giovanbattista01@gmail.com>
2023-01-29 19:45:05 +0100

    salvataggio su json

--- CCM_BotTelegram/Program.cs
+++ CCM_BotTelegram/Program.cs
@@ -1,15 +1,39 @@
-﻿using Telegram.Bot;
+﻿using Newtonsoft.Json;
+using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace CCM_BotTelegram
 {
+    struct BotUpdate
+    {
+        public string type;
+        public string text;
+        public long id;
+        public string? username;
+    }
+
     internal class Program
     {
         static TelegramBotClient Client = new TelegramBotClient(PrivateConfiguration.getToken());
+        static List<BotUpdate> botUpdates = new List<BotUpdate>();
+
         static void Main(string[] args)
         {
+            // Read all saved updates
+            try
+            {
+                var botUpdatesString = System.IO.File.ReadAllText(PrivateConfiguration.getLogFileName());
+
+                botUpdates = JsonConvert.DeserializeObject<List<BotUpdate>>(botUpdatesString) ?? botUpdates;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
             var receiverOptions = new ReceiverOptions()
             { 
                 AllowedUpdates = new UpdateType[]
@@ -32,14 +56,20 @@ namespace CCM_BotTelegram
         {
             if (update.Type == UpdateType.Message)
             {
-                if (update.Message.Type == MessageType.Text)
+                var message_update = new BotUpdate
                 {
-                    string text = update.Message.Text;
-                    long chat_id = update.Message.Chat.Id;
-                    string? username = update.Message.Chat.Username;
+                    type = UpdateType.Message.ToString(),
+                    text = update.Message.Text,
+                    id = update.Message.Chat.Id,
+                    username = update.Message.Chat.Username,
+                };
 
-                    Console.WriteLine($"{username} | {chat_id} | {text}");
-                }
+                // Write an update
+                botUpdates.Add(message_update);
+
+                var message_update_string = JsonConvert.SerializeObject(botUpdates);
+
+                System.IO.File.WriteAllText(PrivateConfiguration.getLogFileName(), message_update_string);
             }
         }
     }
