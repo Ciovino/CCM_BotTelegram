5b639419136eff5bca324dde0d210a91244c78f9
Ciovino <tarantino.giovanbattista01@gmail.com>
2023-01-29 19:36:29 +0100

    semplice lettura dei messaggi

--- CCM_BotTelegram/Program.cs
+++ CCM_BotTelegram/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace CCM_BotTelegram
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace CCM_BotTelegram
 {
     internal class Program
     {
+        static TelegramBotClient Client = new TelegramBotClient(PrivateConfiguration.getToken());
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var receiverOptions = new ReceiverOptions()
+            { 
+                AllowedUpdates = new UpdateType[]
+                {
+                    UpdateType.Message
+                }
+            };
+
+            Client.StartReceiving(UpdateHandler, ErrorHandler, receiverOptions);
+
+            Console.ReadLine();
+        }
+
+        private static Task ErrorHandler(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static async Task UpdateHandler(ITelegramBotClient bot, Update update, CancellationToken token)
+        {
+            if (update.Type == UpdateType.Message)
+            {
+                if (update.Message.Type == MessageType.Text)
+                {
+                    string text = update.Message.Text;
+                    long chat_id = update.Message.Chat.Id;
+                    string? username = update.Message.Chat.Username;
+
+                    Console.WriteLine($"{username} | {chat_id} | {text}");
+                }
+            }
         }
     }
 }
