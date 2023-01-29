79fdc04513e0152fbadc83843f6a2eef1b30e0e0
Ciovino <tarantino.giovanbattista01@gmail.com>
2023-01-29 19:27:11 +0100

    Add project files.

---
+++ CCM_BotTelegram/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CCM_BotTelegram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
